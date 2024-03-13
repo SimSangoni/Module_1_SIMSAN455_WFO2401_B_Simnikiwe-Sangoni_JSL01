00d979140d28605c37abdb56fa22d686f41658ad
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-03-13 11:16:01 +0200

    checking for numericals

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
-    let regex = /[a-zA-Z]+$/; //Checking if a string contains only alphabetic characters
+    let alpha_regex = /[a-zA-Z]+$/; //Checking if a string contains alphabetic characters
+    let num_regex = /[0-9]/; // Checking if the string contains numerical characters
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-        if (input.startsWith("pet_") && regex.test(input)) {
+        if (input.startsWith("pet_") && alpha_regex.test(input) && num_regex_regex.test(input)) {
             result = 'Valid Syntax 🟢';
         } else {
             result = 'Invalid Syntax 🔴';
