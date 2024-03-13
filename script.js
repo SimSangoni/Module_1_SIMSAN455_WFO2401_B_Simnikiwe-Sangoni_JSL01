a8dc0ec648bcccbdf2e1ef4633065efe801c996d
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-03-13 11:14:10 +0200

    checking alphabets now without the start with added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
-    let regex = /^[a-zA-Z]+$/; //Checking if a string contains only alphabetic characters
+    let regex = /[a-zA-Z]+$/; //Checking if a string contains only alphabetic characters
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
