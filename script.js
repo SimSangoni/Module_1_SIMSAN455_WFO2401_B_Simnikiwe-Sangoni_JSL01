fa7782b898abcc89e9384d04f4bcfdc2f8582f92
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-03-13 11:11:34 +0200

    adding condition for regular expression

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
+    let regex = /^[a-zA-Z]+$/; //Checking if a string contains only alphabetic characters
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-        if (input.startsWith("pet_")) {
+        if (input.startsWith("pet_") && regex.test(input)) {
             result = 'Valid Syntax 🟢';
         } else {
             result = 'Invalid Syntax 🔴';
