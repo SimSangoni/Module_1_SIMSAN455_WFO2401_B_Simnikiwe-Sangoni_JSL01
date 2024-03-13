b772841047a0009637262d9c7ae3de4450ade7ac
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-03-13 10:59:07 +0200

    condition for valid and invalid syntax--- start-with only so long (no alphanumerical condition yet)

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        if (input.startsWith("pet_")) {
+            result = 'Valid Syntax 🟢';
+        } else {
+            result = 'Invalid Syntax 🔴';
+        }
 
             document.getElementById('result').innerText = result;
 }
